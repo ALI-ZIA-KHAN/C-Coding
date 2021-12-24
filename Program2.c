ccc406723d11dd126bb413f50205102c7c99f3c0
ALI-ZIA -KHAN <azk443.443@gmail.com>
2021-12-25 03:13:46 +0500

    Basic Data Type

---
+++ Program2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void main(){
+
+    int a=5;
+    double b=6;
+    char c='a';
+    int f;
+
+    printf("The value of a is :%d\n",a);
+    printf("The value of b is :%d\n",b);
+    printf("The value of c is :%c\n",c);
+    printf("The size of int data type  in byte is :%d",sizeof(f));
+};
